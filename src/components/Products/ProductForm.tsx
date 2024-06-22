03ceae365a687c56c38d8f95236ad47af537cab2
= <=>
2024-06-21 19:45:09 -0500

    feat: change error form

--- src/components/Products/ProductForm.tsx
+++ src/components/Products/ProductForm.tsx
@@ -30,7 +30,7 @@ export default function ProductsForm({
   return (
     <form onSubmit={handleSubmit(onSubmit)} noValidate>
       <div className="col-span-2 lg:col-span-1">
-        <div className="flex flex-col mb-6 mr-3">
+        <div className="flex flex-col mb-4 mr-3">
           <label htmlFor="title" className="text-blue-50 font-semibold mb-2">
             Nombre*
           </label>
@@ -42,9 +42,10 @@ export default function ProductsForm({
             name="title"
             className=" text-gray-500  shadow rounded-xl border-2 border-blue-50 disabled:border-gray-400 p-2  focus-visible:outline-none  focus-visible:border-blue-50"
           />
-          {errors.title && (
-            <p className="text-red-500 text-sm">{errors.title.message}</p>
-          )}
+
+          <span className="text-red-500 text-sm">
+            {errors?.title?.message ?? " "}
+          </span>
         </div>
         <div className="flex flex-col mb-6 mr-3">
           <label htmlFor="category" className="text-blue-50 font-semibold mb-2">
@@ -64,7 +65,7 @@ export default function ProductsForm({
             ))}
           </select>
         </div>
-        <div className="flex flex-col mb-6 mr-3">
+        <div className="flex flex-col mb-4 mr-3">
           <label
             htmlFor="description"
             className="text-blue-50 font-semibold mb-2">
@@ -78,12 +79,13 @@ export default function ProductsForm({
             rows={4}
             className="text-gray-500  shadow rounded-xl border-2 border-blue-50 disabled:border-gray-400 p-2  focus-visible:outline-none  focus-visible:border-blue-50"
           />
-          {errors.description && (
-            <p className="text-red-500 text-sm">{errors.description.message}</p>
-          )}
+
+          <span className="text-red-500 text-sm">
+            {errors?.description?.message ?? " "}
+          </span>
         </div>
 
-        <div className="flex flex-col mb-6 ml-3">
+        <div className="flex flex-col mb-4 ml-3">
           <label
             htmlFor="price"
             className="text-blue-50 font-semibold mb-2 -ml-3">
@@ -101,9 +103,10 @@ export default function ProductsForm({
               className="w-full text-gray-500  shadow rounded-xl border-2 border-blue-50 disabled:border-gray-400 py-2 pr-1 pl-7  focus-visible:outline-none  focus-visible:border-blue-50"
             />
           </div>
-          {errors.price && (
-            <p className="text-red-500 text-sm">{errors.price.message}</p>
-          )}
+
+          <span className="text-red-500 text-sm">
+            {errors?.price?.message ?? " "}
+          </span>
         </div>
       </div>
       <button ref={submitRef} type="submit" className="invisible" />
